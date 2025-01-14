24930cb1e41a82dc4cf68e2aa1bf2bef25546bd3
ninditaeka <ninditaeka@gmail.com>
2025-01-14 18:59:23 +0700

    implement basic auth

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,11 +1,117 @@
-export const authUser = () => {
-    
-}
+import { compare, genSalt, hash } from 'bcrypt';
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+import { sign } from 'jsonwebtoken';
 
-export const registerUser = () => {
-    
-}
+// export const authUser = () => {
 
-export const loginUser = () => {
-    
-}
+// }
+const prisma = new PrismaClient();
+
+export const loginProcess = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const findUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+
+    const passCompare = await compare(password, String(findUser?.password));
+
+    if (!passCompare) {
+      throw new Error('invalid email or password');
+    }
+    //jwt
+    const jwtPayload = { email: findUser?.email, role: findUser?.role };
+    const token = sign(jwtPayload, String(process.env.JWT_KEY));
+
+    if (findUser) {
+      res.status(200).json({
+        status: 'success',
+        message: 'login success',
+        data: {
+          token: token,
+          role: findUser?.role,
+        },
+      });
+    } else {
+      res.status(400).json({
+        status: 'bad request',
+        message: 'email or password invalid',
+        data: null,
+      });
+    }
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: JSON.stringify(err),
+      data: null,
+    });
+  }
+};
+
+export const registerProcess = async (req: Request, res: Response) => {
+  try {
+    const { first_name, last_name, email, password, role } = req.body;
+
+    const checkUser = await prisma.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (checkUser) {
+      res.status(400).json({
+        status: 'email already used',
+        data: null,
+      });
+      return;
+    }
+    const salt = await genSalt(10);
+
+    const passCrypt = await hash(password, salt);
+
+    const register = await prisma.user.create({
+      data: {
+        first_name: first_name,
+        last_name: last_name,
+        email: email,
+        role: role,
+        password: passCrypt,
+      },
+    });
+
+    if (!first_name || !last_name || !password || !role) {
+      res.status(400).send('all fields are required');
+    }
+
+    if (!['event_organizer', 'customer'].includes(role)) {
+      res.status(400).send('invalid type');
+    }
+
+    if (first_name || last_name || password || email || role) {
+      res.status(200).json({
+        status: 'success register!!!',
+        data: {
+          first_name: first_name,
+          last_name: last_name,
+          email: email,
+          role: role,
+        },
+      });
+    }
+
+    // if (role == "event_organizer") {
+    //   res.status(201).send("event organizer registed success");
+    // } else {
+    //   res.status(201).send("customer registered successfully");
+    // }
+  } catch (err) {
+    res.status(500).json({
+      status: 'error',
+      message: JSON.stringify(err),
+      data: null,
+    });
+  }
+};
--- apps/api/src/routers/sample.router.ts
+++ apps/api/src/routers/sample.router.ts
@@ -1,20 +1,20 @@
-import { SampleController } from '@/controllers/sample.controller';
+// import { SampleController } from '@/controllers/sample.controller';
 import { Router } from 'express';
 
 export class SampleRouter {
   private router: Router;
-  private sampleController: SampleController;
+  // private sampleController: SampleController;
 
   constructor() {
-    this.sampleController = new SampleController();
+    // this.sampleController = new SampleController();
     this.router = Router();
     this.initializeRoutes();
   }
 
   private initializeRoutes(): void {
-    this.router.get('/', this.sampleController.getSampleData);
-    this.router.get('/:id', this.sampleController.getSampleDataById);
-    this.router.post('/', this.sampleController.createSampleData);
+    // this.router.get('/', this.sampleController.getSampleData);
+    // this.router.get('/:id', this.sampleController.getSampleDataById);
+    // this.router.post('/', this.sampleController.createSampleData);
   }
 
   getRouter(): Router {
