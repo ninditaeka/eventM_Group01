cc01da973a5f7101a24c410d394bc806ab63927d
ninditaeka <ninditaeka@gmail.com>
2025-01-15 17:29:33 +0700

    add validation LogIn and Register in BE

---
+++ apps/api/src/middlewares/authValidation.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from 'express';
+import { body, validationResult } from 'express-validator';
+
+export const LogInValidate = [
+  body('email').notEmpty().withMessage('email is empty'),
+  body('password').notEmpty().withMessage('password is empty'),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: 'error',
+        message: errors.array(),
+        data: null,
+      });
+      return;
+    }
+    next();
+  },
+];
+
+export const RegisterValidate = [
+  body('first_name')
+    .notEmpty()
+    .withMessage('first name is empty')
+    .isAlphanumeric()
+    .withMessage('first name must be alphanumeric'),
+
+  body('last_name')
+    .notEmpty()
+    .withMessage('last name is empty')
+    .isAlphanumeric()
+    .withMessage('last name must be alphanumeric'),
+
+  body('email').notEmpty().withMessage('email is empty'),
+
+  body('password').notEmpty().withMessage('password is empty'),
+
+  body('role')
+    .notEmpty()
+    .withMessage('role is empty')
+    .isAlphanumeric()
+    .withMessage('role must be alphanumeric'),
+
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        status: 'error',
+        message: errors.array(),
+        data: null,
+      });
+      return;
+    }
+    next();
+  },
+];
+//akses validate
--- apps/api/src/routers/auth.router.ts
+++ apps/api/src/routers/auth.router.ts
@@ -1,9 +1,12 @@
-import { Router } from "express";
-import { registerProcess, loginProcess } from "../controllers/auth.controller";
-
+import { Router } from 'express';
+import { registerProcess, loginProcess } from '../controllers/auth.controller';
+import { RegisterValidate, LogInValidate } from '../middlewares/authValidation';
 const router = Router();
 
-router.post("/login", loginProcess);
-router.post("/register", registerProcess);
+router.post('/login', LogInValidate, loginProcess);
+router.post('/register', RegisterValidate, registerProcess);
+
+// router.post('/login', loginProcess);
+// router.post('/register', registerProcess);
 
 export default router;
