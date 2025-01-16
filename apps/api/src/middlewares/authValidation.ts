c789300fa97ffeb19520d76f837a681d014d0759
ninditaeka <ninditaeka@gmail.com>
2025-01-16 19:44:23 +0700

    edit auth validation

--- apps/api/src/middlewares/authValidation.ts
+++ apps/api/src/middlewares/authValidation.ts
@@ -20,17 +20,9 @@ export const LogInValidate = [
 ];
 
 export const RegisterValidate = [
-  body('first_name')
-    .notEmpty()
-    .withMessage('first name is empty')
-    .isAlphanumeric()
-    .withMessage('first name must be alphanumeric'),
+  body('first_name').notEmpty().withMessage('first name is empty'),
 
-  body('last_name')
-    .notEmpty()
-    .withMessage('last name is empty')
-    .isAlphanumeric()
-    .withMessage('last name must be alphanumeric'),
+  body('last_name').notEmpty().withMessage('last name is empty'),
 
   body('email').notEmpty().withMessage('email is empty'),
 
