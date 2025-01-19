7b86c60a45aec81c5bae291cda6702fef518ae42
DitaAuliaIlanafari <ditailanafari@gmail.com>
2025-01-19 11:17:08 +0700

    fixing layout login and register

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -8,34 +8,31 @@ import Link from 'next/link';
 
 export default function Login() {
   return (
-    <div className="flex px-2 md:px-10 md:mt-20 md:flex-row py-28 md:py-16">
-      <div className="md:px-56 flex-col px-10 md:mt-10 mt-10">
-        <h1 className="text-pink-600 font-extrabold text-2xl md:text-2xl">
+    <div className="flex px-2 flex-col md:flex-row h-[100vh]">
+      <div className="md:px-40 flex-col content-center px-10 mt-10 lg:w-1/2">
+        <h1 className="text-red-400 font-extrabold text-2xl md:text-2xl">
           EventBuzz
         </h1>
         <h1 className="text-black md:mt-10 mt-6 text-4xl font-extrabold md:text-4xl">
           Log in
         </h1>
 
-        <form className=" w-72 md:w-auto  mt-10 ">
+        <form className="w-auto mt-10 ">
           <div className="mb-5">
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="Email"
               required
             />
           </div>
           <div className="mb-5">
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Password
-            </label>
             <input
               type="password"
               id="password"
               placeholder="Password"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               required
             />
           </div>
@@ -43,15 +40,15 @@ export default function Login() {
 
           <button
             type="submit"
-            className="text-white bg-pink-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+            className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
           >
             Log in
           </button>
         </form>
-        <div className="mt-4 md:mt-4  md:mb-8  mb-2 text-xs ">
+        <div className="mt-4 md:mt-4  md:mb-8  mb-2 text-base ">
           Don't have account?
           <Link
-            className="mt-6 md:mt-8 text-xs font-extrabold text-pink-600  hover:underline"
+            className="mt-6 md:mt-8 text-base font-extrabold text-red-400  hover:underline"
             href={'/register'}
           >
             {' '}
@@ -59,11 +56,11 @@ export default function Login() {
           </Link>
         </div>
       </div>
-      <div className=" flex flex-row md:w-1/2  invisible md:visible md:justify-end">
+      <div className="hidden md:flex flex-row md:w-1/2 md:justify-end">
         <Image
-          className="rounded-lg shadow-xl dark:shadow-gray-800"
-          width={900}
-          height={1000}
+          className="h-full w-full object-center object-cover rounded-lg shadow-xl dark:shadow-gray-800"
+          width={718}
+          height={404}
           src="/logIn.jpg"
           alt="login"
         />
--- apps/web/src/app/register/page.tsx
+++ apps/web/src/app/register/page.tsx
@@ -8,21 +8,21 @@ import Link from 'next/link';
 
 export default function Register() {
   return (
-    <div className="flex px-2 md:px-10 md:mt-20 md:flex-row py-28 md:py-16">
-      <div className="md:px-56 flex-col px-10 md:mt-10 mt-10">
-        <h1 className="text-pink-600 font-extrabold text-2xl md:text-2xl">
+    <div className="flex px-2 md:flex-row h-[100vh]">
+      <div className="md:px-40 flex-col content-center mx-10 mt-10 w-full lg:w-1/2">
+        <h1 className="text-red-400 font-extrabold text-2xl md:text-2xl">
           EventBuzz
         </h1>
-        <h1 className="text-black md:mt-10 mt-6 text-4xl font-extrabold md:text-4xl">
+        <h1 className="text-black md:mt-6 mt-4 text-4xl font-extrabold md:text-4xl">
           Register
         </h1>
 
-        <form className=" md:w-auto w-72 mt-10 ">
+        <form className="w-auto mt-10 ">
           <div className="mb-5">
             <input
               type="firstName"
               id="firstName"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="First Name"
               required
             />
@@ -32,7 +32,7 @@ export default function Register() {
             <input
               type="lastName"
               id="lastName"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="Last Name"
               required
             />
@@ -42,7 +42,7 @@ export default function Register() {
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="Email"
               required
             />
@@ -51,7 +51,7 @@ export default function Register() {
             <input
               type="password"
               id="password"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="Password"
               required
             />
@@ -60,25 +60,24 @@ export default function Register() {
             <input
               type="role"
               id="role"
-              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-pink-600 focus:border-pink-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
+              className="bg-gray-50 border border-black text-gray-900 text-sm rounded-sm focus:ring-red-400 focus:border-red-400 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-pink-600 dark:focus:border-pink-600"
               placeholder="Role"
               required
             />
           </div>
-          <div className="flex items-start mb-5"></div>
 
           <button
             type="submit"
-            className="text-white bg-pink-600 hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
+            className="text-white bg-red-400 hover:bg-red-500 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"
           >
             Submit
           </button>
         </form>
 
-        <div className="mt-4 md:mt-4  md:mb-8  mb-2 text-xs ">
+        <div className="mt-4 md:mt-4  md:mb-8  mb-2 text-base ">
           Have account?
           <Link
-            className="mt-6 md:mt-8 text-xs font-extrabold text-pink-600  hover:underline"
+            className="mt-6 md:mt-8 text-base font-extrabold text-red-400  hover:underline"
             href={'/login'}
           >
             {' '}
@@ -86,11 +85,11 @@ export default function Register() {
           </Link>
         </div>
       </div>
-      <div className=" flex flex-row md:w-1/2  invisible md:visible md:justify-end">
+      <div className=" hidden md:flex flex-row md:w-1/2 md:justify-end">
         <Image
-          className="rounded-lg   shadow-xl dark:shadow-gray-800"
-          width={900}
-          height={1200}
+          className="h-full w-full object-center object-cover rounded-lg shadow-xl dark:shadow-gray-800"
+          width={718}
+          height={404}
           src="/register.jpg"
           alt="register"
         />
