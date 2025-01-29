2acba1f19aa30cb1a1fd57b7f97328289046a0a5
DitaAuliaIlanafari <ditailanafari@gmail.com>
2025-01-29 23:01:15 +0700

    update image process

--- apps/api/src/routers/event.router.ts
+++ apps/api/src/routers/event.router.ts
@@ -18,66 +18,19 @@ import { eventValidate } from '@/middlewares/eventValidation';
 // import {multer} from 'multer';
 import { uploader } from 'uploader';
 
-//untuk menambahkan path
-const multer = require('multer');
-const path = require('path');
-
-const diskStorage = multer.diskStorage({
-  destination: (req: Request, file: any, cb: any) => {
-    cb(null, path.join(__dirname, 'public/images'));
-  },
-  filename: (req: Request, file: any, cb: any) => {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
-    );
-  },
-});
-
-const upload = multer({
-  dest: 'public/images',
-  storage: diskStorage,
-  limits: {
-    fileSize: 1024 * 1024 * 2, // Max file size 2MB
-  },
-});
-
 const router = Router();
-// router.post('/form', upload.single('file'), createEventTwo);
-// router.post('/form', upload.single('file'), (req, res) => {
-//   res.status(200).json({
-//     status: 'sucess',
-//     message: JSON.stringify({}),
-//     data: null,
-//   });
-// });
-
-// router.post('/upload', upload.single('file'), (req, res) => {
-//   console.log(req.file);
-//   if (!req.file) {
-//     return res.status(400).send('No file uploaded');
-//   }
-//   res.send('File uploaded successfully');
-// });
 
 router.get('/', getEvents);
 router.delete('/:id', verifyToken, eventOrganizerGuard, deleteEvent);
 router.patch('/:id', verifyToken, eventOrganizerGuard, editEvent);
 router.get(
   '/:id',
-  verifyToken,
-  eventOrganizerGuard,
-  participantGuard,
+  // verifyToken,
+  // eventOrganizerGuard,
+  // participantGuard,
   getEventById,
 );
-router.post(
-  '/',
-  verifyToken,
-  eventOrganizerGuard,
-  eventValidate,
-  uploader('IMG', 'event-images').single('file'),
-  createEvent,
-);
+router.post('/', verifyToken, eventOrganizerGuard, eventValidate, createEvent);
 router.get('/user/:created_by', verifyToken, getEventByUserId);
 router.get('/search', searchEvents);
 
