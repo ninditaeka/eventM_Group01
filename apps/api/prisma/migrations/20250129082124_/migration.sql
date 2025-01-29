84268b9b066596c3e0650dd94e076feb63665966
ninditaeka <ninditaeka@gmail.com>
2025-01-29 17:45:23 +0700

    cahnge in prisma model

---
+++ apps/api/prisma/migrations/20250129082124_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `discount_coupon_use` on the `checkouts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "checkouts" DROP COLUMN "discount_coupon_use";
---
+++ apps/api/prisma/migrations/20250129104406_change_image_from_dbvarchar_become_string_text_aja/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "events" ALTER COLUMN "image" SET DATA TYPE TEXT;
