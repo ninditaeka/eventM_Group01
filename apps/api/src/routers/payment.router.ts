f71be8c31c98f3d5e0651c65fc3ee56d6fb7c1ae
ninditaeka <ninditaeka@gmail.com>
2025-01-29 17:46:22 +0700

    payment controller be

---
+++ apps/api/src/routers/payment.router.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import {
+  createPayment,
+  getPaymenttById,
+} from '@/controllers/payment.controller';
+import { participantGuard } from '@/middlewares/auth.middleware';
+import { paymentValidate } from '@/middlewares/paymentValidation';
+
+const router = Router();
+
+router.post('/', participantGuard, paymentValidate, createPayment);
+// router.post('/', verifyToken, checkoutValidate, validateCheckout);
+// router.get('/', getEvents);
+// router.delete('/:id', verifyToken, deleteEvent);
+// router.patch('/:id', verifyToken, editEvent);
+router.get('/:id', getPaymenttById);
+
+export default router;
