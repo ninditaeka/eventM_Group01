fbfd5cf6790647626d427c7ec5d24a9317371e7d
ninditaeka <ninditaeka@gmail.com>
2025-01-30 21:58:16 +0700

    add use client

--- apps/web/src/services/user.ts
+++ apps/web/src/services/user.ts
@@ -4,6 +4,8 @@ import axios from 'axios';
 import { profile } from 'console';
 import Cookies from 'js-cookie';
 
+import { editEventByEO, getEventListbyId } from './event';
+
 const BASE_URL = 'http://localhost:8000';
 
 const profileData = async (id: string) => {
