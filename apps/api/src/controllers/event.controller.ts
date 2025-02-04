38e3da2bbec4db93012fb752dd12b9cc917e3784
ninditaeka <ninditaeka@gmail.com>
2025-02-04 19:09:08 +0700

    event

--- apps/api/src/controllers/event.controller.ts
+++ apps/api/src/controllers/event.controller.ts
@@ -13,9 +13,6 @@ type User = {
   id: string;
 };
 
-// // const multer = require('multer');
-// const path = require('path');
-
 export const createEvent = async (req: Request, res: Response) => {
   const {
     title,
@@ -31,25 +28,8 @@ export const createEvent = async (req: Request, res: Response) => {
   } = req.body;
 
   const user = req.user as User;
-  console.log(req.body);
+  console.log('req body', req.body);
   try {
-    // console.log({
-    //   data: {
-    //     title: title || '',
-    //     description: description || '',
-    //     image: image || '',
-    //     location: location || '',
-    //     date: date || '',
-    //     event_type: event_type || '',
-    //     price: price || '',
-    //     total_seat: total_seat || '',
-    //     total_transaction_discount: total_transaction_discount || '',
-    //     category: category || '',
-    //     created_by: parseInt(user.id),
-    //   },
-    // });
-
-    // Use current time if date is not provided
     const eventDate = date ? new Date(date) : new Date(); // Use current time if date is not provided
 
     // Log the event date before insertion
@@ -114,112 +94,6 @@ export const createEvent = async (req: Request, res: Response) => {
   }
 };
 
-// const diskStorage = multer.diskStorage({
-//   destination: (req: Request, file: any, cb: any) => {
-//     cb(null, path.join(__dirname, 'public/images'));
-//   },
-//   filename: (req: Request, file: any, cb: any) => {
-//     cb(
-//       null,
-//       `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`,
-//     );
-//   },
-// });
-
-// const upload = multer({
-//   storage: diskStorage,
-//   limits: {
-//     fileSize: 1024 * 1024 * 25, // Max file size 25MB
-//   },
-// });
-
-export const createEventTwo = async (req: Request, res: Response) => {
-  // upload(req, res, (err) => {
-  //   if (err) {
-  //     console.log('upload error');
-  //   } else {
-  //     console.log('upload ok');
-  //   }
-  // });
-  // const {
-  //   title,
-  //   description,
-  //   image,
-  //   location,
-  //   date,
-  //   event_type,
-  //   price,
-  //   total_seat,
-  //   total_transaction_discount,
-  //   category,
-  // } = req.body;
-
-  // const user = req.user as User;
-  console.log('req.body: ', req.body);
-  console.log('req.file: ', req.file);
-  // try {
-  //   ({
-  //     data: {
-  //       title: title || '',
-  //       description: description || '',
-  //       image: image || '',
-  //       location: location || '',
-  //       date: date || '',
-  //       event_type: event_type || '',
-  //       price: price || 0,
-  //       total_seat: total_seat || 0,
-  //       total_transaction_discount: total_transaction_discount || 0,
-  //       category: category || '',
-  //       created_by: parseInt(user.id),
-  //     },
-  //   });
-
-  //   const newPost = await prisma.event.create({
-  //     data: {
-  //       title: title || '',
-  //       description: description || '',
-  //       image: image || '',
-  //       location: location || '',
-  //       date: new Date(date) || '',
-  //       event_type: event_type || '',
-  //       price: parseInt(price) || 0,
-  //       total_seat: parseInt(total_seat) || 0,
-  //       total_transaction_discount: parseInt(total_transaction_discount) || 0,
-  //       category: category || '',
-  //       created_by: parseInt(user.id),
-  //     },
-  //   });
-
-  //   const priceLocal = price.toLocaleString();
-  //   const timeLocal = moment
-  //     .utc(newPost.date)
-  //     .tz('Asia/Jakarta')
-  //     .format('YYYY-MM-DD HH:mm:ss');
-
-  //   const outputData = { ...newPost };
-
-  //   console.log({ ...outputData, date: timeLocal, price: priceLocal });
-
-  //   res.status(201).json({
-  //     status: 'success',
-  //     message: 'create event success',
-  //     data: { ...outputData, date: timeLocal, price: priceLocal },
-  //   });
-  // } catch (err) {
-  //   res.status(500).json({
-  //     status: 'error',
-  //     message: JSON.stringify(err),
-  //     data: null,
-  //   });
-  // }
-
-  res.status(200).json({
-    status: 'sucess',
-    message: JSON.stringify({}),
-    data: null,
-  });
-};
-
 export const getEvents = async (req: Request, res: Response) => {
   try {
     // Get query category
@@ -260,28 +134,6 @@ export const getEvents = async (req: Request, res: Response) => {
   }
 };
 
-// export const deleteEvent = async (req: Request, res: Response) => {
-//   try {
-//     const { id } = req.params;
-
-//     const deleteEvent = await prisma.event.delete({
-//       where: {
-//         id: Number(id),
-//       },
-//     });
-
-//     res.status(200).json({
-//       status: 'delete success',
-//       data: deleteEvent,
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       status: 'error',
-//       message: JSON.stringify(err),
-//     });
-//   }
-// };
-
 export const getEventById = async (req: Request, res: Response) => {
   try {
     const id = Number(req.params.id);
@@ -317,28 +169,6 @@ export const getEventById = async (req: Request, res: Response) => {
 
     console.log('availableSeats:', availableSeats);
 
-    // TODO: event.totalSeat - count dari checkout yang is paid true
-
-    //   const data = await prisma.$queryRaw`
-    //   WITH dataTransaction AS (
-    //     SELECT
-    //       u.first_name,
-    //       u.last_name,
-    //       e.title,
-    //       e.id AS eventId,
-    //       c.id AS co_id,
-    //       c.created_at
-    //     FROM users u
-    //     JOIN checkouts c ON u.id = c."userId"
-    //     JOIN events e ON c."eventId" = e.id
-    //     WHERE c."eventId" = ${id}
-    //   )
-    //   SELECT dt.*, p.is_paid
-    //   FROM dataTransaction dt
-    //   LEFT JOIN payments p
-    //   ON dt.co_id = p."checkoutId";
-    // `;
-    // console.log('data: ', data);
     if (!event) {
       res.status(400).json({
         status: 'event not found',
@@ -362,7 +192,6 @@ export const getEventById = async (req: Request, res: Response) => {
 
 export const getEventByEoId = async (req: Request, res: Response) => {
   try {
-    // const id = Number(req.params.created_by);
     const user = req.user as User;
 
     if (isNaN(parseInt(user.id))) {
